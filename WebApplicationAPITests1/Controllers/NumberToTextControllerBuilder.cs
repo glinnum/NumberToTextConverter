0cbb54317d69ecc863d79796e8cf5c57907edb55
Christian Glinnum <Christian@crgservice.dk>
2020-11-22 10:37:05 +0100

    Added builder

---
+++ WebApplicationAPITests1/Controllers/NumberToTextControllerBuilder.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using NumberToTextConverter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebApplicationAPI.Controllers;
+using Moq;
+
+namespace WebApplicationAPITests.Controllers
+{
+    public class NumberToTextControllerBuilder
+    {
+        private NumberToTextController _NumberToTextController;
+        private Mock<ILogger<NumberToTextController>> _mockLogger;
+        private Mock<IMyConverter> _mockMyConverter;
+
+        public NumberToTextControllerBuilder()
+        {
+        }
+                public NumberToTextControllerBuilder Default()
+        {
+            _mockMyConverter = new Mock<IMyConverter>();
+            _mockLogger = new Mock<ILogger<NumberToTextController>>();
+            return this;
+        }
+
+        public NumberToTextControllerBuilder WithMyConverter(Mock<IMyConverter> myConverter)
+        {
+            _mockMyConverter = myConverter;
+            return this;
+        }
+
+        public NumberToTextControllerBuilder WithLogger(Mock<ILogger<NumberToTextController>> logger)
+        {
+            _mockLogger = logger;
+            return this;
+        }
+
+        public NumberToTextController Build()
+        {
+            _NumberToTextController = new NumberToTextController(
+            _mockLogger.Object,
+            _mockMyConverter.Object);
+            return _NumberToTextController;
+        }
+    }
+}
